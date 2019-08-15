5f219d0248f9b9a4a2b1658883244e24fe7128c0
DaniTox <danitox@protonmail.com>
2019-08-15 23:55:02 +0200

    Added PercorsoFormativo raw files

---
+++ app/src/main/java/com/danitox/igio_android/CompagniaAgent.kt
@@ -0,0 +1,11 @@
+package com.danitox.igio_android
+
+import android.content.Context
+import android.content.res.AssetManager
+
+class CompagniaAgent(val context: Context) {
+
+    fun test() {
+        val iss = context.assets.open("")
+    }
+}
