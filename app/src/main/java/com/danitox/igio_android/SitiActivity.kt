c055b78905252c8da5d2aa0e957ead20547e301c
DaniTox <danitox@protonmail.com>
2019-10-20 14:18:43 +0200

    rewrote SitiActivity

--- app/src/main/java/com/danitox/igio_android/SitiActivity.kt
+++ app/src/main/java/com/danitox/igio_android/SitiActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.graphics.Canvas
 import android.net.Uri
 import android.os.Bundle
+import android.support.design.widget.Snackbar
 import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.DividerItemDecoration
@@ -13,6 +14,10 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.Item
+import com.xwray.groupie.Section
+import com.xwray.groupie.ViewHolder
 import kotlinx.android.synthetic.main.locations_activity.tableView
 import kotlinx.android.synthetic.main.siti_activity.*
 import kotlinx.android.synthetic.main.sito_row.view.*
@@ -20,7 +25,10 @@ import kotlinx.android.synthetic.main.social_layout.*
 
 class SitiActivity : Fragment() {
 
-    private var sitesAdapter: SitiAdapter? = null
+    val sitiCategorie : List<SitoCategoria> = listOf(SitoCategoria.materiali, SitoCategoria.preghiere)
+
+    val sitiAgent: SitiLocalizer = SitiLocalizer()
+    var allSites: List<SitoObject> = listOf()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.list_view, container, false)
@@ -29,35 +37,76 @@ class SitiActivity : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        this.sitesAdapter = SitiAdapter(mutableListOf(SitoCategoria.materiali, SitoCategoria.preghiere)) { sito ->
-            val intent = Intent(Intent.ACTION_VIEW)
-            intent.data = Uri.parse(sito.urlString)
-            this.startActivity(intent)
-        }
-
         val divider = DividerItemDecoration(this.context, DividerItemDecoration.VERTICAL)
         this.tableView.addItemDecoration(divider)
         this.tableView.layoutManager = LinearLayoutManager(this.context)
-        this.tableView.adapter = sitesAdapter
 
-        this.sitesAdapter?.updateHandler = {
-            this.activity?.runOnUiThread {
-                this.sitesAdapter?.notifyDataSetChanged()
-            }
-        }
-
-        this.sitesAdapter?.errorHandler = {
-            Log.e("Sites", it)
-        }
-
-
-        this.sitesAdapter?.updateFromServer()
+        updateFromServer()
     }
 
 
     override fun onResume() {
         super.onResume()
-        sitesAdapter?.fetchLocalWebsites()
+        fetchLocalWebsites()
+    }
+
+    fun fetchLocalWebsites() {
+        val list : MutableList<SitoObject> = mutableListOf()
+        for (categoria in this.sitiCategorie) {
+            list.addAll(sitiAgent.fetchLocalWebsites(categoria))
+        }
+        this.allSites = list.toList()
+        fillTableview()
+    }
+
+    fun updateFromServer() {
+        sitiAgent.fetchAllWebsites { list, error ->
+            if (error == null && list != null) {
+                this.allSites = list.siti.filter { this.sitiCategorie.contains(it.type) }.toList()
+                fillTableview()
+            } else {
+                showError(error!!)
+            }
+        }
+    }
+
+    fun fillTableview() {
+        this.activity?.runOnUiThread {
+            val adapter = GroupAdapter<ViewHolder>()
+
+            //val mainSection = Section(ToxHeader("Risorse"))
+            val mainSection = Section()
+            for (sito in allSites) {
+                val newRow = SitoItem(sito) { sitoClickedAction(it) }
+                mainSection.add(newRow)
+            }
+
+            adapter.add(mainSection)
+            tableView.adapter = adapter
+        }
+    }
+
+    fun sitoClickedAction(sito: SitoObject) {
+        val intent = Intent(Intent.ACTION_VIEW)
+        intent.data = Uri.parse(sito.urlString)
+        this.startActivity(intent)
+    }
+
+    fun showError(message: String) {
+        Snackbar.make(this.tableView, message, Snackbar.LENGTH_LONG).show()
+    }
+}
+
+class SitoItem(val sito: SitoObject, var clickAction: ((SitoObject) -> Unit)?): Item<ViewHolder>() {
+
+    override fun bind(viewHolder: ViewHolder, position: Int) {
+        viewHolder.itemView.sitoNameLabel.text = sito.name
+
+        viewHolder.itemView.setOnClickListener { clickAction?.invoke(sito) }
+    }
+
+    override fun getLayout(): Int {
+        return R.layout.sito_row
     }
 }
 
